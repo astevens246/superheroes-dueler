4995b8afc18c54d5048be0b4a5e7fdeb19770098
Allen Stevens <allen.stevens@students.dominican.edu>
2023-11-04 14:03:10 -0700

    added dog class

--- dog.py
+++ dog.py
@@ -1,4 +1,14 @@
 class Dog: 
-    def __init__(self, name):
+    def __init__(self, name, breed):
         self.name = name
+        self.breed = breed
         print("dog initialized!")
+  # Methods are defined as their own named functions inside the class
+  # Remember to put the "self" parameter every time we make a class method!def bark(self):
+    def bark(self):   
+        print("Woof!")
+# the same instantiation call that creates a Dog object,
+# but now we've assigned it to the value of the my_dog variable
+
+my_dog = Dog("Rex","Superdog")
+
---
+++ my_dogs.py
@@ -0,0 +1,4 @@
+import dog #imports the memory of the dog class from dog.py
+
+my_other_dog = dog.Dog("Annie", "SuperDog")
+print(my_other_dog.name)                   
